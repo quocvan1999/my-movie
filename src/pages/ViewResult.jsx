24139181e235099dcca89f48ee14d0413a5593df
Quocvan <lyquocvan1999@gmail.com>
2024-07-30 22:51:52 +0700

    update reponsive movie item

--- src/pages/ViewResult.jsx
+++ src/pages/ViewResult.jsx
@@ -4,7 +4,7 @@ import VideoList from "../components/VideosComponent/VideoList";
 const ViewResult = () => {
   return (
     <div className="w-full">
-      <div className="w-full h-[300px] bg-[url(../../../../public/images/movie/breadcrumb-bg.webp)] bg-cover bg-no-repeat">
+      <div className="w-full h-[300px] md:h-[400px] lg:h-[500px] bg-[url(../../../../public/images/movie/breadcrumb-bg.webp)] bg-cover bg-no-repeat">
         <div className="w-full h-full bg-[#00000094] flex justify-center items-center">
           <h1 className="text-white font-medium text-4xl">View All</h1>
         </div>
