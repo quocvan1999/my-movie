cb4e596ff39d0fe6bbf8350076b8e434e680a919
Quocvan <lyquocvan1999@gmail.com>
2024-07-30 22:29:30 +0700

    update reponsive movie item

--- src/components/VideosComponent/VideoList.jsx
+++ src/components/VideosComponent/VideoList.jsx
@@ -6,27 +6,27 @@ import VideoItem from "./VideoItem";
 
 const responsive = {
   superLargeDesktop: {
-    breakpoint: { max: 4000, min: 3000 },
+    breakpoint: { max: 4000, min: 1000 },
     items: 5,
   },
   desktop: {
-    breakpoint: { max: 3000, min: 1024 },
-    items: 5,
+    breakpoint: { max: 1000, min: 768 },
+    items: 4,
   },
   tablet: {
-    breakpoint: { max: 1024, min: 464 },
-    items: 5,
+    breakpoint: { max: 768, min: 480 },
+    items: 3,
   },
   mobile: {
-    breakpoint: { max: 464, min: 0 },
-    items: 1,
+    breakpoint: { max: 480, min: 0 },
+    items: 2,
   },
 };
 
 const VideoList = ({ title, type, all = true }) => {
   return (
     <div className="max-w-[1280px] mx-auto py-2">
-      <div className="flex justify-between">
+      <div className="flex justify-between px-3">
         <h1 className="text-white uppercase text-2xl leading-7">{title}</h1>
         {all && (
           <NavLink to="/viewresult/viewall" className="text-[#E40813]">
