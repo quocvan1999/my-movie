0b772ab8c9f936e381c537cefde73e1e95312d98
Quocvan <lyquocvan1999@gmail.com>
2024-08-01 20:02:46 +0700

    setup redux - store

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,8 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 
+import { store } from "./redux/store";
+import { Provider } from "react-redux";
+
 import HomePage from "./layouts/HomePage";
 import Home from "./pages/Home";
 import Detail from "./pages/Detail";
@@ -9,24 +12,26 @@ import ViewResult from "./pages/ViewResult";
 function App() {
   return (
     <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<HomePage />}>
-          <Route index element={<Home />} />
-          <Route path="detail">
-            <Route path=":id" element={<Detail />} />
+      <Provider store={store}>
+        <Routes>
+          <Route path="/" element={<HomePage />}>
+            <Route index element={<Home />} />
+            <Route path="detail">
+              <Route path=":id" element={<Detail />} />
+            </Route>
+            <Route path="streamvideo">
+              <Route path=":id" element={<StreamVideo />} />
+            </Route>
+            <Route path="viewresult">
+              <Route path="viewall" element={<ViewResult />} />
+              <Route path="phimbo" element={<ViewResult />} />
+              <Route path="phimle" element={<ViewResult />} />
+              <Route path="tvshow" element={<ViewResult />} />
+              <Route path="hoathinh" element={<ViewResult />} />
+            </Route>
           </Route>
-          <Route path="streamvideo">
-            <Route path=":id" element={<StreamVideo />} />
-          </Route>
-          <Route path="viewresult">
-            <Route path="viewall" element={<ViewResult />} />
-            <Route path="phimbo" element={<ViewResult />} />
-            <Route path="phimle" element={<ViewResult />} />
-            <Route path="tvshow" element={<ViewResult />} />
-            <Route path="hoathinh" element={<ViewResult />} />
-          </Route>
-        </Route>
-      </Routes>
+        </Routes>
+      </Provider>
     </BrowserRouter>
   );
 }
