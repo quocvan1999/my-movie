83be3695c8bc0bb335e6ca78b056540a95fdec29
Quocvan <lyquocvan1999@gmail.com>
2024-08-06 19:36:21 +0700

    update

--- src/pages/StreamVideo.jsx
+++ src/pages/StreamVideo.jsx
@@ -1,28 +1,50 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
 import { useDispatch, useSelector } from "react-redux";
-import { setDetailMovieAsync } from "../redux/apiRequests/apiRequests";
+import {
+  setDetailMovieAsync,
+  getMoviesInTypeAsync,
+} from "../redux/apiRequests/apiRequests";
 
 import StreamContent from "../components/StreamVideo/StreamContent";
 import VideoContainer from "../components/StreamVideo/VideoContainer";
 import EpisodeList from "../components/StreamVideo/EpisodeList";
 import Loading from "../components/Loading";
+import VideoList from "../components/VideosComponent/VideoList";
 
 const StreamVideo = () => {
   const { id } = useParams();
   const dispatch = useDispatch();
+  const [isDetail, setIsDetail] = useState(false);
   const { detailMovie, detailMoviePending } = useSelector(
     (state) => state.detailMovieReducer
+  );
+  const { phimTheoTheLoai, phimTheoTheLoaiPending } = useSelector(
+    (state) => state.phimTheoTheLoaiReducer
   );
 
   const setDetailMovieApi = async (id) => {
     const action = setDetailMovieAsync(id);
     dispatch(action);
+    setIsDetail(true);
+  };
+
+  const getMoviesInTypeApi = async () => {
+    if (detailMoviePending === false && detailMovie && detailMovie?.movie) {
+      const action = getMoviesInTypeAsync(detailMovie?.movie.category);
+      dispatch(action);
+    }
   };
 
   useEffect(() => {
     setDetailMovieApi(id);
   }, [id]);
+
+  useEffect(() => {
+    if (isDetail === true) {
+      getMoviesInTypeApi();
+    }
+  }, [detailMoviePending, detailMovie]);
 
   return (
     <>
@@ -38,6 +60,19 @@ const StreamVideo = () => {
             <div className="mb-10">
               <EpisodeList />
             </div>
+          </div>
+          <div>
+            {phimTheoTheLoaiPending === false &&
+            phimTheoTheLoai &&
+            phimTheoTheLoai?.length > 0 ? (
+              phimTheoTheLoai.map((item, index) => {
+                return (
+                  <VideoList data={item.data} key={index} type="caroucel" />
+                );
+              })
+            ) : (
+              <Loading />
+            )}
           </div>
         </div>
       ) : (
